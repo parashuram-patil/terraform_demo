4e0a15f560600ef835215577f3f7147818455b78
parashuram-patil <parasharam_patil@ymail.com>
2019-05-23 18:53:52 +0530

    Querying AWS S3 lambda function and Outputting its Handler name
    >terraform apply

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ data "aws_s3_bucket" "psptfs3bucketdata" {
   bucket = "psptfs3bucket"
 }
 
+data "aws_lambda_function" "psptflambdadata" {
+  function_name = "pspJavafunction"
+}
+
 output "psptfs3bucketdataout" {
   value = "${aws_s3_bucket.psptfs3bucket.region}"
+}
+
+output "psptflambdadataout" {
+  value = "${data.aws_lambda_function.psptflambdadata.handler}"
 }
