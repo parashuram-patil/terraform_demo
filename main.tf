7bb0196795d5e60a777d77a2c4b37f0d34d4a21c
parashuram-patil <parasharam_patil@ymail.com>
2019-05-23 19:22:17 +0530

    creating AWS MySQL DB Instance
    >terraform apply

--- main.tf
+++ main.tf
@@ -28,6 +28,18 @@ resource "aws_s3_bucket" "psptfs3bucket" {
   bucket = "psptfs3bucket"
 }
 
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "root"
+  password             = "root12345"
+  parameter_group_name = "default.mysql5.7"
+}
+
 data "aws_s3_bucket" "psptfs3bucketdata" {
   bucket = "psptfs3bucket"
 }
